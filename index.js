0d9ad0efb4f2f0ba1bee298292b6ad9b25eba427
RYCKAERT Guillaume <ryckaert.guillaume@neuf.fr>
2022-05-25 20:13:38 +0200

    fix /casual

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ client.on('interactionCreate', interaction => {
       let ncGap=dhm(nextCasual-now);
       let timeStamp = nextCasual.getTime().toString().substring(0,10);
       embed.setTitle('Next casual weekly is in '+ ncGap.days+' days, '+ncGap.hours+' hours, and ' +ncGap.minutes+ ' minutes');
-      embed.setDescription("The race should start on the <t:"+timeStamp+":D>, around <t:"+timeStamp+":t>\nRace room should open 1 hour before.");
+      embed.setDescription("The race should start on the <t:"+timeStamp+":D>, around <t:"+timeStamp+":t>\nRace room should open at some point, when Hendrus feels like it.");
     embed.setColor(0xffad21);
     interaction.reply({embeds:[embed]});
   }
