4eafe7f8c31105c09a460017f43beab7508a6e42
Rylux <ryckaert.guillaume@neuf.fr>
2023-01-07 16:15:49 +0100

    remove console.log

--- src/commands/faqCommand.js
+++ src/commands/faqCommand.js
@@ -5,9 +5,7 @@ const RANDO_INFO_CHANNEL_ID=process.env.RANDO_INFO_ID;
 
 export default function yellariFAQCommand(interaction){
     const embed = new EmbedBuilder();
-    console.log(RANDO_INFO_CHANNEL_ID)
     var channel='<#'+RANDO_INFO_CHANNEL_ID+'>';
-    console.log(channel);
 	embed.setTitle('To get started with the randomizer, please read our FAQ and startup guide.')
 	embed.setDescription('You can find them in '+channel+".");
 	embed.setColor(0xffad21);
