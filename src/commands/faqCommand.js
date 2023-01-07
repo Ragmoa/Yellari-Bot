0ccba30049a5dac6f830672bc94b5f7543f2f91d
Rylux <ryckaert.guillaume@neuf.fr>
2023-01-07 16:12:49 +0100

    fix faq command

--- src/commands/faqCommand.js
+++ src/commands/faqCommand.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config()
 import { EmbedBuilder } from '@discordjs/builders';
-const RANDO_INFO_CHANNEL_ID=process.env.RANDO_INFO_CHANNEL_ID;
+const RANDO_INFO_CHANNEL_ID=process.env.RANDO_INFO_ID;
 
 export default function yellariFAQCommand(interaction){
     const embed = new EmbedBuilder();
+    console.log(RANDO_INFO_CHANNEL_ID)
     var channel='<#'+RANDO_INFO_CHANNEL_ID+'>';
+    console.log(channel);
 	embed.setTitle('To get started with the randomizer, please read our FAQ and startup guide.')
 	embed.setDescription('You can find them in '+channel+".");
 	embed.setColor(0xffad21);
